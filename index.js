d02101b3c09df09c57dbbf624ab706e336d1b786
chillbuilds <will@chillbuilds.com>
2024-11-10 11:23:44 -0700

    updated express

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 const express = require('express')
-const path = require('path')
-const app = express()
-const PORT = process.env.PORT || 3000
+    const path = require('path')
+    const app = express()
+    const port = process.env.PORT || 3000
 
-app.use(express.json())
+    app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.get('/test', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'html/index.html'))
-})
-
-app.use((req, res) => {
-    res.status(404).send('Not Found')
-})
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, './public/html/index.html'))
+    })
+    
+    app.listen(port, () => {
+        console.log('server started')
+    })
