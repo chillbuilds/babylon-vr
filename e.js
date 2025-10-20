6932a4c3bd169c851e2345ec9aec1392e8374f9b
chillbuilds <will@chillbuilds.com>
2025-10-19 19:35:33 -0600

    added backend logging test

--- e.js
+++ e.js
@@ -1,26 +1,28 @@
 const express = require('express')
-    const path = require('path')
-    const app = express()
-    const port = process.env.PORT || 3000
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 3000
 
-    app.use(express.json())
-    app.use(express.static(path.join(__dirname, 'public')))
+console.log('log test')
 
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, './public/index.html'))
-    })
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
 
-    app.get('/button-test', (req, res) => {
-        console.log('button pushed')
-        res.sendStatus(200)
-    })
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
-    app.post('/headset-position', (req, res) => {
-        const position = req.body
-        console.log(position)
-        res.sendStatus(200)
-    })
-    
-    app.listen(port, () => {
-        console.log('server started')
-    })
+app.get('/button-test', (req, res) => {
+    console.log('button pushed')
+    res.sendStatus(200)
+})
+
+app.post('/headset-position', (req, res) => {
+    const position = req.body
+    console.log(position)
+    res.sendStatus(200)
+})
+
+app.listen(port, () => {
+    console.log('server started')
+})
